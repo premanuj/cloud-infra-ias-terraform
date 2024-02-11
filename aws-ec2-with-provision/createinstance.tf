7d1af9ec4b795d3602790f6a54424df84b630fdc
premanuj <mesubedianuj@gmail.com>
2024-02-11 23:13:38 +0545

    Revert "2 ec2 with provisioning terraform"

--- aws-ec2-with-provision/createinstance.tf
+++ aws-ec2-with-provision/createinstance.tf
@@ -10,11 +10,6 @@ data "aws_ami" "ubuntu"{
   //query = "sort_by(Images, &CreationDate)[].Name"
 }
 
-resource "aws_key_pair" "ec2key"{
-    key_name = "ec2key"
-    public_key = file(var.PATH_TO_PUBLIC_KEY)
-  }
-
 
 resource "aws_instance" "ec2instance" {
   ami = data.aws_ami.ubuntu.id
@@ -23,24 +18,4 @@ resource "aws_instance" "ec2instance" {
   tags = {
     Name: "Test instance"
   }
-
-  provisioner "file" {
-      source = "install_nginx.sh"
-      destination = "/tmp/install_nginx.sh"
-    }
-  
-  provisioner "remote-exec" {
-      inline = [
-        "chmod +x /tmp/install_ngnix.sh",
-        "sudo /tmp/install_nginx.sh"
-      ]
-    }
-
-    connection {
-        host = coalesce(self.public_ip, self.private_ip)
-        type = "ssh"
-        user = var.INSTANCE_USERNAME
-        private_key = file(var.PATH_TO_PRIVATE_KEY)
-      }
 }
-
